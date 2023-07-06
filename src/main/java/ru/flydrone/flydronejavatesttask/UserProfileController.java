44cf24ce681521f1353e117c8adb6c3062b7d685
Natalie Kazantseva <kazantseva@flydrone.ru>
2023-07-06 21:05:38 +0300

    добавила валидацию

--- src/main/java/ru/flydrone/flydronejavatesttask/UserProfileController.java
+++ src/main/java/ru/flydrone/flydronejavatesttask/UserProfileController.java
@@ -1,9 +1,15 @@
 package ru.flydrone.flydronejavatesttask;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class UserProfileController {
@@ -15,10 +21,23 @@ public class UserProfileController {
     }
 
     @PostMapping("/api/save-user-profile")
-    public ResponseEntity<Long> saveUserProfile(@RequestBody UserProfileDTO userProfile) {
+    public ResponseEntity<Long> saveUserProfile(@RequestBody @Valid UserProfileDTO userProfile) {
         Long userProfileId = service.saveUserProfile(userProfile);
         var status = userProfile.getId() == null ? HttpStatus.CREATED : HttpStatus.OK;
         //return service.saveUserProfile(userProfile);
         return ResponseEntity.status(status).body(userProfileId);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
     }
 }
--- src/main/java/ru/flydrone/flydronejavatesttask/UserProfileDTO.java
+++ src/main/java/ru/flydrone/flydronejavatesttask/UserProfileDTO.java
@@ -1,13 +1,21 @@
 package ru.flydrone.flydronejavatesttask;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
 import java.util.Date;
-import java.util.Optional;
 
 public class UserProfileDTO {
     private Long id;
+    @NotBlank(message = "lastName cant't be empty")
     private final String lastName;
+    @NotBlank(message = "firstName cant't be empty")
     private final String firstName;
     private final String patronymic;
+    @NotNull(message = "birthdate cant't be empty")
+    @Max((LocalDateTime.now()).minusMonths(12 * 18))
     private final Date birthdate;
 
     public UserProfileDTO(Long id, String lastName, String firstName, String patronymic, Date birthdate) {
